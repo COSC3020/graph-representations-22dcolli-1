17c257cefee05d9bbbdde20b793f6a310a18c7cf
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-11-13 02:12:53 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function convertToAdjList(adjMatrix) {
+    let adjList = []
+    for(let i = 0; u < adjMatrix.length; i++){
+        adjList[i] = [];
+
+        for (let j = 0; j < adjMatrix.length;j++ ){
+            if(adjMatrix[i][j] == 1){
+                adjList[i].push[j];
+            }
+        }
+    }
     return adjMatrix;
 }
